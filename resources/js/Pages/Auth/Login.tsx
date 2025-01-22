ff431b0de95918adcc290bf7d5b1b8d0427224df
mits2025 <michi2002.ferraren@gmail.com>
2025-01-23 01:45:17 +0800

    Install Filament and Panel

---
+++ resources/js/Pages/Auth/Login.tsx
@@ -0,0 +1,109 @@
+import Checkbox from '@/Components/Core/Checkbox';
+import InputError from '@/Components/Core/InputError';
+import InputLabel from '@/Components/Core/InputLabel';
+import PrimaryButton from '@/Components/Core/PrimaryButton';
+import TextInput from '@/Components/Core/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
+import { Head, Link, useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+
+export default function Login({
+                                  status,
+                                  canResetPassword,
+                              }: {
+    status?: string;
+    canResetPassword: boolean;
+}) {
+    const { data, setData, post, processing, errors, reset } = useForm<{
+        email: string;
+        password: string;
+        remember: boolean;
+    }>({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+
+        post(route('login'), {
+            onFinish: () => reset('password'),
+        });
+    };
+
+    return (
+        <AuthenticatedLayout>
+            <Head title="Log in" />
+            <div className="flex h-screen">
+                {/* Left Side */}
+                <div className="flex-grow bg-gray-100"></div>
+
+                {/* Right Side (Log In Form) */}
+                <div className="w-1/2 bg-white flex items-center justify-center">
+                    <div className="p-8 w-full max-w-lg">
+                        {status && (
+                            <div className="mb-4 text-sm font-medium text-green-600">
+                                {status}
+                            </div>
+                        )}
+
+                        <form onSubmit={submit} className="space-y-6">
+                            <div>
+                                <InputLabel htmlFor="email" value="Email" />
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) => setData('email', e.target.value)}
+                                />
+                                <InputError message={errors.email} className="mt-2" />
+                            </div>
+
+                            <div>
+                                <InputLabel htmlFor="password" value="Password" />
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full"
+                                    autoComplete="current-password"
+                                    onChange={(e) => setData('password', e.target.value)}
+                                />
+                                <InputError message={errors.password} className="mt-2" />
+                            </div>
+
+                            <div className="flex items-center">
+                                <Checkbox
+                                    name="remember"
+                                    checked={data.remember}
+                                    onChange={(e) => setData('remember', e.target.checked)}
+                                />
+                                <span className="ml-2 text-sm text-gray-600">
+                            Remember me
+                        </span>
+                            </div>
+
+                            <div className="flex items-center justify-between">
+                                {canResetPassword && (
+                                    <Link href={route('password.request')} className="text-blue-500 hover:underline">
+                                        Forgot your password?
+                                    </Link>
+                                )}
+                                <PrimaryButton disabled={processing}>Log in</PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+
+    );
+}
+
