ecf53f734a3cf7c703957c994a68b84ef8efdb67
mits2025 <michi2002.ferraren@gmail.com>
2025-02-13 16:20:08 +0800

    addition of vendor request

--- app/Enum/VendorStatusEnum.php
+++ app/Enum/VendorStatusEnum.php
@@ -7,4 +7,22 @@ enum VendorStatusEnum: string
     case Pending = 'pending';
     case Approved = 'approved';
     case Rejected = 'rejected';
+
+    public static function labels(): array
+    {
+        return [
+            self::Pending->value => __('Pending'),
+            self::Approved->value => __('Approved'),
+            self::Rejected->value => __('Rejected'),
+        ];
+    }
+    public static function colors(): array
+    {
+        return [
+            self::Pending->value => 'gray',    // Pending status -> gray color
+            self::Approved->value => 'success', // Approved status -> success color
+            self::Rejected->value => 'danger',  // Rejected status -> danger color
+        ];
+    }
 }
+
