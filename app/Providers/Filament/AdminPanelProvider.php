49a4d903f156f610d0588a151800814cfd984209
mits2025 <michi2002.ferraren@gmail.com>
2025-02-14 14:51:48 +0800

    addition of vendor request and profile vendor

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->sidebarWidth('21rem')
             ->login()
             ->colors([
-                'primary' => Color::Green,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -54,10 +54,7 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
                 'auth',
-                sprintf('role:%s|%s',
-                    RolesEnum::Admin->value,
-                    RolesEnum::Vendor->value,
-                )
+                'vendor.approved' => \App\Http\Middleware\EnsureVendorIsApproved::class,
             ]);
     //        ->authMiddleware([
     //            Authenticate::class,
